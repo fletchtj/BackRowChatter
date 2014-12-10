95bd16c94bcfc082883b44c5f1974cdf7d7de5a5
TJ Fletcher <fletcher93@gmail.com>
2014-12-09 21:12:18 -0600

    updated Meteor and fixed name issue when users are missing profile name

--- packages/utils/template_helpers.js
+++ packages/utils/template_helpers.js
@@ -1,101 +1,25 @@
 if (Meteor.isClient) {
-	var DateFormats = {
-		"short": "MMM DD, YYYY",
-		"shortdt": "MMM, DD @ h:mm a",
-		"medium": "dddd DD.MM.YYYY HH:mm",
-		"long": "dddd, MMMM DD @ h:mm a",
-		"abbr": "ddd, MMM D, YYYY",
-		"datepicker": "MM/DD/YYYY",
-		"military": "HH:mm"
-	}
 
 	Template.registerHelper("createdBy", function () {
-		var _userId = Meteor._get(this, "created", "by") || null
-			, user = Meteor.users.findOne(_userId)
-			, name;
-		if (user) {
-			name = user.profile.name;
-		} else {
-			name = "Unknown User";
-		}
-		return name;
+		var userId = Meteor._get(this, "created", "by") || null
+		return Utils.objects.userName(userId);
 	});
 
 	Template.registerHelper("createdOn", function (format, showDefaultText) {
-		var datetime = Meteor._get(this, "created", "on")
-			, d = new Date(datetime)
-			, _dateString
-			, format = format || "long";
+		var datetime = Meteor._get(this, "created", "on");
+		return Utils.dates.formatDate(datetime, format, showDefaultText);
+	});
 
-		showDefaultText = _.isBoolean(showDefaultText) ? showDefaultText : false;
-		
-		if (!datetime) {
-			return showDefaultText && "Not specified" || "";
-		}
-		
-		if (Package["mrt:moment"]) {
-			if (format === "relative") {
-				_dateString = moment(d).fromNow();
-			} else {
-				var f = DateFormats[format];
-				_dateString = moment(d).format(f);
-			}
-		} else {
-			// return basic mm/dd/yyyy format if momentjs not found
-			_dateString = (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear();
-		}
-		return _dateString;
+	Template.registerHelper("userName", function (userId) {
+		return Utils.objects.userName(userId);
 	});
 
 	Template.registerHelper("formatDate", function (datetime, format, showDefaultText) {
-		// console.log(datetime, format);
-		showDefaultText = _.isBoolean(showDefaultText) ? showDefaultText : false;
-		if (!datetime) {
-			return showDefaultText && "Not specified" || "";
-		}
-		var d = new Date(datetime);
-		var _dateString;
-		if (Package["mrt:moment"]) {
-			if (format === "relative") {
-				_dateString = moment(d).fromNow();
-			} else {
-				var f = DateFormats[format];
-				_dateString = moment(d).format(f);
-			}
-		} else {
-			// return basic mm/dd/yyyy format if momentjs not found
-			_dateString = (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear();
-		}
-		return _dateString;
+		return Utils.dates.formatDate(datetime, format, showDefaultText);
 	});
 
 	Template.registerHelper("formatDuration", function (duration, format, showDefaultText) {
-		showDefaultText = _.isBoolean(showDefaultText) ? showDefaultText : false;
-		if (!duration) {
-			return showDefaultText && "Not specified" || "";
-		}
-
-		// set default return value
-		var _dur = duration / (1000 * 3600 * 24) + " days";
-
-		if (Package["mrt:moment"]) {
-			switch (format) {
-				case "human":
-					_dur = moment.duration(duration).humanize();
-					break;
-				case "days":
-					_dur = +moment.duration(duration).asDays().toFixed(0);
-					_dur = pluralize(_dur, "day");
-					break;
-				case "weeks":
-					_dur = +moment.duration(duration).asWeeks().toFixed(1);
-					_dur = pluralize(_dur, "week");
-					break;
-				default:
-					_dur = moment.duration(duration).humanize();
-			}
-		}
-		return _dur;
+		return Utils.dates.formatDuration(duration, format, showDefaultText);
 	});
 
 	Template.registerHelper("formatString", function (text, format) {
@@ -114,18 +38,6 @@ if (Meteor.isClient) {
 			}
 		}
 		return _text;
-	});
-
-	Template.registerHelper("userName", function (userId) {
-		var _userId = userId || Meteor.userId()
-			, user = Meteor.users.findOne(_userId)
-			, name;
-		if (user) {
-			name = user.profile.name;
-		} else {
-			name = "Unknown User";
-		}
-		return name;
 	});
 
 	Template.registerHelper("pluralize", function(n, thing, plural) {
--- packages/utils/utils.js
+++ packages/utils/utils.js
@@ -1,3 +1,13 @@
+var DateFormats = {
+	"short": "MMM DD, YYYY",
+	"shortdt": "MMM, DD @ h:mm a",
+	"medium": "dddd DD.MM.YYYY HH:mm",
+	"long": "dddd, MMMM DD @ h:mm a",
+	"abbr": "ddd, MMM D, YYYY",
+	"datepicker": "MM/DD/YYYY",
+	"military": "HH:mm"
+};
+
 Utils = {
 	strings: {
 		capitalize: function (s) {
@@ -56,8 +66,72 @@ Utils = {
 			return o;
 		}
 	}
+	, dates: {
+		formatDate: function (ts, format, showDefaultText) {
+			var d = new Date(ts)
+				, _dateString
+				, format = format || "long";
+
+			showDefaultText = _.isBoolean(showDefaultText) ? showDefaultText : false;
+			
+			if (!ts) {
+				return showDefaultText && "Not specified" || "";
+			}
+			
+			if (Package["mrt:moment"]) {
+				if (format === "relative") {
+					_dateString = moment(d).fromNow();
+				} else {
+					var f = DateFormats[format];
+					_dateString = moment(d).format(f);
+				}
+			} else {
+				// return basic mm/dd/yyyy format if momentjs not found
+				_dateString = (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear();
+			}
+			return _dateString;
+		}
+		, formatDuration: function (duration, format, showDefaultText) {
+			showDefaultText = _.isBoolean(showDefaultText) ? showDefaultText : false;
+			if (!duration) {
+				return showDefaultText && "Not specified" || "";
+			}
+
+			// set default return value
+			var _dur = duration / (1000 * 3600 * 24) + " days";
+
+			if (Package["mrt:moment"]) {
+				switch (format) {
+					case "human":
+						_dur = moment.duration(duration).humanize();
+						break;
+					case "days":
+						_dur = +moment.duration(duration).asDays().toFixed(0);
+						_dur = pluralize(_dur, "day");
+						break;
+					case "weeks":
+						_dur = +moment.duration(duration).asWeeks().toFixed(1);
+						_dur = pluralize(_dur, "week");
+						break;
+					default:
+						_dur = moment.duration(duration).humanize();
+				}
+			}
+			return _dur;
+		}
+	}
 	, objects: {
-		generateAuditStamp: function () {
+		userName: function (userId) {
+			var user = Meteor.users.findOne(userId)
+				, name;
+			if (user) {
+				name = user.profile.name || user.username;
+			} else {
+				name = "Unknown User";
+			}
+			return name;
+		}
+		, generateAuditStamp: function () {
 			// used for 'created' and 'lastModified'...
 			return { "on": new Date().getTime(), "by": Meteor.userId() }
 		}
